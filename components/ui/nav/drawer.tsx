5b6495f0be6b8f4f18bdf97a396edee902e530ed
Armando Gavrila <armando.gavrila@emag.ro>
2022-11-09 17:35:39 +0200

    mobile sections

--- components/ui/nav/drawer.tsx
+++ components/ui/nav/drawer.tsx
@@ -8,24 +8,25 @@ export default function Drawer(props) {
   const [open, setOpen] = useState(false);
 
   const openDrawerHandler = () => {
-    console.log(open);
     setOpen(!open);
   };
 
   return (
-    <div className={styles.drawer}>
-      <div className={styles.drawerButton}>
-        <NavButton border goTo={openDrawerHandler}>
-          <MenuIcon />
-        </NavButton>
+    <div>
+      <div className={styles.drawer}>
+        <div className={styles.drawerButton}>
+          <NavButton border goTo={openDrawerHandler}>
+            <MenuIcon />
+          </NavButton>
+        </div>
+        {open ? (
+          <DrawerContent
+            buttons={props.buttons}
+            goTo={props.goTo}
+            closeDrawer={openDrawerHandler}
+          />
+        ) : null}
       </div>
-      {open ? (
-        <DrawerContent
-          buttons={props.buttons}
-          goTo={props.goTo}
-          closeDrawer={openDrawerHandler}
-        />
-      ) : null}
     </div>
   );
 }
--- components/ui/nav/navbar.tsx
+++ components/ui/nav/navbar.tsx
@@ -7,8 +7,8 @@ export default function Navbar(props) {
   return (
     <NavbarContainer>
       <Logo />
-      <HorizontalNav buttons={props.buttons} goTo={props.goTo} />
       <Drawer open buttons={props.buttons} goTo={props.goTo} />
+      <HorizontalNav buttons={props.buttons} goTo={props.goTo} />
     </NavbarContainer>
   );
 }
