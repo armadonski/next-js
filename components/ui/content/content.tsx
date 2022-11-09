59dd20edbaa738351d43065cd6f70d51ed665ff9
Armando Gavrila <armando.gavrila@gmail.com>
2022-11-09 12:15:48 +0200

    animated content

--- components/ui/content/content.tsx
+++ components/ui/content/content.tsx
@@ -1,8 +1,15 @@
 import styles from "../../../styles/Content.module.css";
+import { animated, useSpring } from "react-spring";
 
 export default function Content(props) {
+  const navStyle = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    delay: Math.floor(Math.random() * 1000),
+  });
+
   return (
-    <div>
+    <animated.div style={navStyle}>
       <div className={styles.title}>
         <h1>
           <span className="coloredText">0{props.number}</span>. {props.title}
@@ -10,6 +17,6 @@ export default function Content(props) {
         <div className={`${styles.horizontalLine} lineColor`}></div>
       </div>
       {props.children}
-    </div>
+    </animated.div>
   );
 }
