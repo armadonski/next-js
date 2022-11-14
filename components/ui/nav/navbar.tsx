52b6393afaf3a45d4361b877a9db74212143e0ec
Armando Gavrila <armando.gavrila@emag.ro>
2022-11-14 14:12:42 +0200

    removed logo

--- components/ui/nav/navbar.tsx
+++ components/ui/nav/navbar.tsx
@@ -6,7 +6,6 @@ import Drawer from "./drawer";
 export default function Navbar(props: { buttons: object; goTo: Function; }) {
   return (
     <NavbarContainer>
-      <Logo />
       <Drawer open buttons={props.buttons} goTo={props.goTo} />
       <HorizontalNav buttons={props.buttons} goTo={props.goTo} />
     </NavbarContainer>
